04e234055d88db5c8f0dbbd501f57d39132608eb
Allana <allanac.avila@gmail.com>
2023-02-06 15:42:06 -0300

    banco adicionado

---
+++ Banco/Banco/Filtrar/Filtrando.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banco.Filtrar
+{
+    internal class Filtrando
+    {
+        //Método para imprimir todos os construtores da classe
+        public static void ImprimeConstrutores(Type pTipo)
+        {
+            Console.WriteLine("Imprimindo os Construtores da Classe System.Type");
+            Console.WriteLine(" ");
+
+            ConstructorInfo[] construtores = pTipo.GetConstructors();
+
+            Console.WriteLine("Existe(m)" + construtores.Length + " construtores.");
+            Console.WriteLine(" ");
+
+            foreach(ConstructorInfo construtor in construtores)
+            {
+                Console.WriteLine("Nome = " + construtor.Name);
+                ParameterInfo[] parametros = construtor.GetParameters();
+
+                if(parametros.Length > 0)
+                {
+                    Console.WriteLine("parametros do construtor");
+
+                    foreach(ParameterInfo parametro in parametros)
+                    {
+                        Console.WriteLine("Posição: " + parametro.Position);
+                        Console.WriteLine("Nome: " + parametro.Name);
+                        Console.WriteLine("Tipo: " + parametro.GetType());
+                        Console.WriteLine(" ");
+                    }
+                    Console.WriteLine("========================================== ");
+                    Console.WriteLine(" ");
+                }
+            }
+        }
+    }
+}
---
+++ Banco/Banco/Program.cs
@@ -0,0 +1,51 @@
+﻿
+using Microsoft.CodeAnalysis;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Banco.Contas;
+using Banco.Titular;
+using Banco.Filtrar;
+using System.Reflection;
+
+
+namespace Banco
+{
+    class ClasseBanco
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente filtro = new ContaCorrente();
+
+            Type tipo = typeof(ContaCorrente);
+
+            if (!tipo.IsAbstract)
+            {
+                Console.WriteLine("Classe ContaCorrente tem os seguintes construtores: ");
+                filtro.ImprimeConstrutores(tipo);
+            }
+
+            //Imprime todos os metodos
+            filtro.ImprimeMetodos(tipo);
+
+            //Imprime todas as propriedades
+            filtro.ImprimePropriedades(tipo);
+
+            //Imprime todos os campos
+            //filtro.ImprimeCampos(tipo);
+
+            //Imprime todos as interfaces
+            //filtro.ImprimeInterfaces(tipo);
+
+            //Imprime os atributos
+            filtro.ImprimeAtributos(tipo);
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
